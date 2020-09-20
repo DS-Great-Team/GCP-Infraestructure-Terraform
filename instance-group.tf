a25604cc34002a957b69dc6a224ecfe4ee2fcf66
danielitogomez <daniel.gomez.amaran@gmail.com>
2020-09-19 22:36:22 -0300

    volviendo el target a 1

--- instance-group.tf
+++ instance-group.tf
@@ -8,7 +8,7 @@ resource "google_compute_region_instance_group_manager" "bastion" {
   region                    = var.gcp_region_1
   distribution_policy_zones = var.availability_zones
 
-  target_size = 2
+  target_size = 1
 
 }
 
